a6f95784d067580ecb4667b601fc2c1b1a7a1eef
Ariel D'Alessandro <ariel.dalessandro@gmail.com>
2023-04-03 18:16:11 -0300

    Add hl7 controller
    Signed-off-by: Ariel D'Alessandro <ariel.dalessandro@gmail.com>

---
+++ src/hl7/hl7.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { HL7Message } from './entities/hl7.entity';
+import { HL7Service } from './hl7.service';
+import { Public } from '../auth/decorators/public.decorator';
+
+@Controller('hl7')
+export class HL7Controller {
+  constructor(private readonly hl7Service: HL7Service) {}
+
+  @Get()
+  findAll(): Promise<HL7Message[]> {
+    return this.hl7Service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<HL7Message> {
+    return this.hl7Service.findOne(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.hl7Service.remove(+id);
+  }
+}
--- src/hl7/hl7.module.ts
+++ src/hl7/hl7.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabasesModule } from '../databases/databases.module';
 import { HL7Providers } from './hl7.providers';
 import { HL7Service } from './hl7.service';
+import { HL7Controller } from './hl7.controller';
 
 @Module({
   imports: [DatabasesModule],
+  controllers: [HL7Controller],
   providers: [...HL7Providers, HL7Service],
   exports: [HL7Service],
 })
