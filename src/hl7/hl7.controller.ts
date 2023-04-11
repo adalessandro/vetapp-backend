c207e9528cf5b5b950f0b858b2d36987629370f2
Ariel D'Alessandro <ariel.dalessandro@gmail.com>
2023-04-11 13:43:00 -0300

    hl7/hl7.controller: Add CRUD for HL7 Entry entities
    Signed-off-by: Ariel D'Alessandro <ariel.dalessandro@gmail.com>

--- src/hl7/hl7.controller.ts
+++ src/hl7/hl7.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Delete } from '@nestjs/common';
-import { HL7Message } from './entities/hl7.entity';
+import { HL7Entry, HL7Message } from './entities/hl7.entity';
 import { HL7Service } from './hl7.service';
+import { HL7EntryService } from './hl7-entry.service';
 import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('hl7')
@@ -20,5 +21,25 @@ export class HL7Controller {
   @Delete(':id')
   remove(@Param('id') id: string): Promise<void> {
     return this.hl7Service.remove(+id);
+  }
+}
+
+@Controller('hl7-entry')
+export class HL7EntryController {
+  constructor(private readonly hl7EntryService: HL7EntryService) {}
+
+  @Get()
+  findAll(): Promise<HL7Entry[]> {
+    return this.hl7EntryService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<HL7Entry> {
+    return this.hl7EntryService.findOne(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.hl7EntryService.remove(+id);
   }
 }
--- src/hl7/hl7.module.ts
+++ src/hl7/hl7.module.ts
@@ -3,11 +3,11 @@ import { DatabasesModule } from '../databases/databases.module';
 import { HL7Providers } from './hl7.providers';
 import { HL7EntryService } from './hl7-entry.service';
 import { HL7Service } from './hl7.service';
-import { HL7Controller } from './hl7.controller';
+import { HL7Controller, HL7EntryController } from './hl7.controller';
 
 @Module({
   imports: [DatabasesModule],
-  controllers: [HL7Controller],
+  controllers: [HL7Controller, HL7EntryController],
   providers: [...HL7Providers, HL7EntryService, HL7Service],
   exports: [HL7EntryService, HL7Service],
 })
